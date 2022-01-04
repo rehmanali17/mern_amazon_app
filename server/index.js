5760c2dfc15afdef3ac8273418fe50fc28a3ff23
rehmanali17 <rehmanmalik17@gmail.com>
2022-01-04 08:37:16 +0500

    Prepared for deployment of Phase 2

--- server/index.js
+++ server/index.js
@@ -1,29 +1,36 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const config = require('config')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const cors = require("cors");
+const path = require("path");
 
-const app = express()
-const PORT = process.env.PORT || config.get('PORT')
+const app = express();
+const PORT = process.env.PORT || config.get("PORT");
 
+app.use(express.json({ extended: true, limit: "5mb" }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public/build")));
 
-app.use(express.json({ extended: true, limit: '5mb' }))
-app.use(cors())
-
-mongoose.connect(config.get('mongoURi'), {useNewUrlParser:true, useUnifiedTopology:true})
-    .then(()=>{
-        console.log('Database Connected')
-        app.listen(PORT, () => console.log(`Server Running at PORT ${PORT}`))
+mongoose
+    .connect(config.get("mongoURi"), {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     })
-    .catch(err => {
-        console.log(err.message)
-        process.exit(1)
+    .then(() => {
+        console.log("Database Connected");
+        app.listen(PORT, () => console.log(`Server Running at PORT ${PORT}`));
     })
+    .catch((err) => {
+        console.log(err.message);
+        process.exit(1);
+    });
 
-app.use('/auth',require('./routes/auth/auth-routes'))    
-app.use('/customer',require('./routes/customer/customer-routes'))    
-app.use('/seller',require('./routes/seller-account/seller-account')) 
-app.use('/currency',require('./routes/currency/currency'))   
-app.use('/sales',require('./routes/sales/sales'))
+app.use("/api/auth", require("./routes/auth/auth-routes"));
+app.use("/api/customer", require("./routes/customer/customer-routes"));
+app.use("/api/seller", require("./routes/seller-account/seller-account"));
+app.use("/api/currency", require("./routes/currency/currency"));
+app.use("/api/sales", require("./routes/sales/sales"));
 
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/build/index.html"));
+});
